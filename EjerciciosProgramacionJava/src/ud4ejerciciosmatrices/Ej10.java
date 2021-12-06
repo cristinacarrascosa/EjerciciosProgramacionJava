cbf0b3ae365e876852626039fd50b6cb612d7ee3
cristinacarrascosa <cctausias@gmail.com>
2021-12-06 22:31:54 +0100

    Ejercicios de Arrays acabados

--- EjerciciosProgramacionJava/src/ud4ejerciciosmatrices/Ej10.java
+++ EjerciciosProgramacionJava/src/ud4ejerciciosmatrices/Ej10.java
@@ -20,7 +20,7 @@ public class Ej10 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-                indexOf
+                //indexOf
     }
 
 }
